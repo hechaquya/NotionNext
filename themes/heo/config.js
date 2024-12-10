a1379e87865043f192315b818eba5c4bfb59983e
hechaquya <lanhuazui@gmail.com>
2024-12-10 20:58:48 +0800

    config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -52,7 +52,7 @@ const CONFIG = {
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
+  //HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://zhaolaoshi.fun',//这里是了解更多跳转的链接
   HEO_INFO_CARD_TEXT3: '了解更多',
  
