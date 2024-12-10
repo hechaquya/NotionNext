f22eb22dc0c8867d8e7e6424ed147e409e680d8d
hechaquya <lanhuazui@gmail.com>
2024-12-10 19:15:03 +0800

    config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到赵老师的网站', url: 'https://zhaolaoshi.fun' },
+    { title: '日拱一卒，功不唐捐', url: 'https://zhaolaoshi.fun' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,10 +18,10 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享数学',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_3: 'ZHAOLAOSHI.FUN',
+  HEO_HERO_TITLE_4: '',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
